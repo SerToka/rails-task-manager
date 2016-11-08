c9e8fc54d6b49c9a46846af5eaa9e001c74aef70
Yvon Rauber <yvonrauber@gmail.com>
2016-11-08 16:18:19 +0100

    ok

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,22 +1,41 @@
 class TaskController < ApplicationController
+
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(set_task_params)
+    redirect_to tasks_path
+
   end
 
   def edit
+     @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(set_task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
+
+  private
+
+  def set_task_params
+    params.require(:task).permit(:date, :name, :done)
 end
--- db/migrate/20161108110630_create_tasks.rb
+++ db/migrate/20161108110630_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
       t.date :date
-      t.string :label
+      t.string :name
       t.boolean :done, default: false
 
       t.timestamps
